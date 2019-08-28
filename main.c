0cdf588a92f72fee13d58b760c1ac4ba9d0f7a08
wanessabezerra <53651524+wanessabezerra@users.noreply.github.com>
2019-08-27 23:28:38 -0300

    Update main.c
    Mudanças nas funções e assinaturas

--- main.c
+++ main.c
@@ -2,60 +2,57 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
-#include <unistd.h>
 
 
 #define quantCadastro 200
 
-
-void menuPrincipal();
-void menuCliente();
-void menuProduto();
-void menuControleDeEstoque();
-void menuRelatorio();
-void menuSobre();
-
-
-void cadastrarCliente();
-void pesquisarCliente();
-void atualizarCliente();
-void deletarCliente();
+void telaMenuPrincipal();
+void telaMenuCliente();
+void telaMenuProduto();
+void telaMenuControleDeEstoque();
+void telaMenuRelatorio();
+void telaMenuSobre();
 
 
-void cadastrarProduto();
-void pesquisarProduto();
-void atualizarProduto();
-void deletarProduto();
+int menuPrincipal();
+int menuCliente();
+int menuProduto();
+int menuControleDeEstoque();
+int menuRelatorio();
+int menuSobre();
 
 
-void cadastrarCompra();
-void pesquisarCompra();
-void atualizarCompra();
+int cadastrarCliente();
+int pesquisarCliente();
+int atualizarCliente();
+int deletarCliente();
 
 
-void cadastrarVenda();
-void pesquisarVenda();
-void atualizarVenda();
+int cadastrarProduto();
+int pesquisarProduto();
+int atualizarProduto();
+int deletarProduto();
 
 
-void relatorioDosClientes();
-void relatorioDosProdutos();
-void relatorioDasCompra();
-void relatorioDasVenda();
+int cadastrarCompra();
+int pesquisarCompra();
+int atualizarCompra();
 
 
-void quemSomos();
-void contato();
+int cadastrarVenda();
+int pesquisarVenda();
+int atualizarVenda();
 
 
-int opcao1;
-int opcao2;
-int opcao3;
-int opcao4;
-int opcao5;
-int opcao6;
-int opcao7; 
+int relatorioDosClientes();
+int relatorioDosProdutos();
+int relatorioDasCompra();
+int relatorioDasVenda();
 
+
+int quemSomos();
+int contato();
+int inforPrograma();
 
 struct cliente {
 		char nomeCliente[quantCadastro] [50];
@@ -63,7 +60,7 @@ struct cliente {
 		char rua[quantCadastro] [50];
 		char email[quantCadastro] [50];
 		int dataDeNascimento[quantCadastro];
-		int  cpf[quantCadastro];
+		int cpf[quantCadastro];
 	};
 
 struct produto {
@@ -84,154 +81,409 @@ struct produto {
 
 
 int main (void){
-	system("cls");
+	int opcao;
+	system("clear");
 	setlocale(LC_ALL, "portuguese");
+	opcao = menuPrincipal();
 
-	do{
-		menuPrincipal();
-		switch(opcao1){
+	while(opcao != 0){
+		switch(opcao){
 			case 1: {
-        system("cls");
-				void menuCliente();
-
+				system("clear");
+				menuCliente();
 				break;}
 
 			case 2:{
-        system("cls");
-				void menuProduto();
-				
+				system("clear");
+				menuProduto();
 				break;}
-	
+
 			case 3: {
-        system("cls");
-				void menuControleDeEstoque();
-				
+				system("clear");
+				menuControleDeEstoque();
 				break;}
-				
+
 			case 4: { 
-        system("cls");
-				void menuRelatorio();
-				
+				system("clear");
+				menuRelatorio();
 				break;}
-			
+
 			case 5: {
-        system("cls");
-				void menuSobre();
-				
+				system("clear");
+				menuSobre();
 				break;}
-			
+
 			case 0: {
-				system("cls");
+				system("clear");
 				return 0;
 				break;}
-				
+
 			default: {
-				system("cls");
+				system("clear");
 				printf("Opção inválida, digite um número válido\n\n");
 				break;}
-			}
-		} while(opcao1 != 0);
+			opcao = menuPrincipal();
+		}
+	}
+}
+
+void telaMenuPrincipal(){
+	printf("\t= = = = = = = = = = = = = = = = = =\n"
+			"\t=   M E N U   P R I N C I P A L   =\n"
+			"\t= = = = = = = = = = = = = = = = = =\n"
+			"\n\t1 - Cliente\n"
+			"\t2 - Produto\n"
+			"\t3 - Controle de Estoque\n"
+			"\t4 - Relatório\n"
+			"\t5 - Sobre\n"
+			"\t0 - Encerrar\n");
+}
+
+void telaMenuCliente(){
+	printf ("\t= = = = = = = = = = = = = = = = = = = = = =\n"
+			"\t=   C A D A S T R O  D O  C L I E N T E   =\n"
+			"\t= = = = = = = = = = = = = = = = = = = = = =\n"
+			  "\n\t1 - Cadastrar Cliente\n"
+			  "\t2 - Atualizar Cliente\n"
+			  "\t3 - Pesquisar Cliente\n"
+			  "\t4 - Deletar Cliente\n"
+			  "\t0 - Retornar\n");
+}
+
+void telaMenuProduto(){
+	printf ("\t= = = = = = = = = = = = = = = = = = = = = =\n"
+			"\t=   C A D A S T R O  D O  P R O D U T O   =\n"
+			"\t= = = = = = = = = = = = = = = = = = = = = =\n"
+			  "\n\t1 - Cadastrar Produto\n"
+			  "\t2 - Atualizar Produto\n"
+			  "\t3 - Pesquisar Produto\n"
+			  "\t4 - Deletar Produto\n"
+			  "\t0 - Retornar\n");
+}
+
+void telaMenuControleDeEstoque(){
+	printf("\t= = = = = = = = = = = = = = = = = = = =\n"
+			"\t= C O N T R O L E  D E  E S T O Q U E =\n"
+			"\t= = = = = = = = = = = = = = = = = = = =\n"
+			  "\n\t1 - Efetuar Compra\n"
+			  "\t2 - Efetuar Venda\n"
+			  "\t3 - Pesquisar Compra\n"
+			  "\t4 - Pesquisar Venda\n"
+			  "\t5 - Atualizar Compra\n"
+			  "\t6 - Atualizar Venda\n"
+			  "\t0 - Retornar\n");
+}
+
+void telaMenuRelatorio(){
+	printf("\t= = = = = = = = = = = = = = \n"
+			"\t=   R E L A T Ó R I O S   =\n"
+			"\t= = = = = = = = = = = = = =\n"
+			  "\n\t1 - Relatório de Clientes\n"
+			  "\t2 - Relatório de Produtos\n"
+			  "\t3 - Relatório de Compras\n"
+			  "\t4 - Relatório de Vendas\n"
+			  "\t0 - Retornar\n");
+}
+
+void telaMenuSobre(){
+  printf ("\t= = = = = = = = = = = = = = = = = = = = = = =\n"
+          "\t=   M U R A L  D E  I N F O R M A Ç Õ E S   =\n"
+          "\t= = = = = = = = = = = = = = = = = = = = = = = \n"
+            "\n\t1 - Quem somos\n"
+            "\t2 - Contato\n"
+            "\t3 - Informações do Programa\n"
+            "\t0 - Retornar\n");
+}
+
+int menuPrincipal(void){
+	int opcao;
+	system("clear");
+	telaMenuPrincipal();
+	printf("\n\tEscolha uma opção: ");
+	scanf("%d", &opcao); 
+	return opcao;
+}
+
+
+
+int menuCliente(){
+	int opcao;
+	system("clear");
+	telaMenuCliente();
+	printf("\n\tEscolha uma opção: ");
+	scanf("%d", &opcao);
+	opcao = menuCliente();
+
+	while(opcao != 0){
+    switch(opcao){
+      case 1: {
+				system("clear");
+				cadastrarCliente();
+        system ("pause");
+				break;}
+
+      case 2: {
+				system("clear");
+				pesquisarCliente();
+        system ("pause");
+				break;}
+      
+      case 3: {
+				system("clear");
+				atualizarCliente();
+				break;}
+      
+      case 4: {
+				system("clear");
+				deletarCliente();
+				break;}
+
+      case 0: {
+				system("clear");
+				return 0;
+				break;}
+
+			default: {
+				system("clear");
+				printf("Opção inválida, digite um número válido\n\n");
+				break;}
+      
+			opcao = menuCliente();
+      return opcao;
+		}
+  }
+}
+
+
+int menuProduto(){
+	int opcao;
+	system("clear");
+	telaMenuProduto();
+	printf("\n\tEscolha uma opção: ");
+	scanf("%d", &opcao);	
+	opcao = menuProduto();
+	while(opcao != 0){
+    switch(opcao){
+      case 1: {
+				system("clear");
+				cadastrarProduto();
+				break;}
+
+      case 2: {
+				system("clear");
+				pesquisarProduto();
+				break;}
+      
+      case 3: {
+				system("clear");
+				atualizarProduto();
+				break;}
+      
+      case 4: {
+				system("clear");
+				deletarProduto();
+				break;}
+
+      case 0: {
+				system("clear");
+				return 0;
+				break;}
+
+			default: {
+				system("clear");
+				printf("Opção inválida, digite um número válido\n\n");
+				break;}
+      
+			opcao = menuCliente();
+      return opcao;
+		}
+  }
+}
+
+int menuControleDeEstoque(){
+	int opcao;
+	system("clear");
+	telaMenuControleDeEstoque();
+	printf("\n\tEscolha uma opção: ");
+	scanf("%d", &opcao);
+	do{
+
+		} while(opcao != 0);
+	return opcao;
+}
+
+int menuRelatorio(){
+	int opcao;
+	system("clear");
+	telaMenuRelatorio();
+	printf("\n\tEscolha uma opção: ");
+	scanf("%d", &opcao);
+	do{
+
+		} while(opcao != 0);
+	return opcao;
+}
+
+int menuSobre(){
+	int opcao;
+	system("clear");
+	telaMenuSobre();
+	printf("\n\tEscolha uma opção: ");
+	scanf("%d", &opcao);
+	do{
+
+		} while(opcao != 0);
+	return opcao;
+}
+
+
+///////////////////////////////////////////
+//// F U N Ç Õ E S  D O  C L I E N T E ////
+///////////////////////////////////////////
+
+
+int cadastrarCliente(){
+	printf("Em Construção");
 	return 0;
 }
 
 
-void menuPrincipal(){
-	system("cls");
-	do{
-		printf ("\t= = = = = = = = = = = = = = = =\n"
-				"\t= M E N U   P R I N C I P A L =\n"
-				"\t= = = = = = = = = = = = = = = =\n"
-				  "\n\t1 - Cadastrar Cliente\n"
-				  "\t2 - Cadastrar Produto\n"
-				  "\t3 - Controle de Estoque\n"
-				  "\t4 - Relatório\n"
-				  "\t5 - Sobre\n"
-				  "\t0 - Encerrar\n");
-		printf("\n\tEscolha uma opção: ");
-		scanf("%d", &opcao2);
-		
-		}while(opcao2 != 0);
-}
-
-void menuCliente(){
-	system("cls");
-	do{
-		printf ("\t= = = = = = = = = = = = = = = = = = = =\n"
-				"\t= C A D A S T R O  D O  C L I E N T E =\n"
-				"\t= = = = = = = = = = = = = = = = = = = =\n"
-				  "\n\t1 - Cadastrar Cliente\n"
-				  "\t2 - Atualizar Cliente\n"
-				  "\t3 - Pesquisar Cliente\n"
-				  "\t4 - Deletar Cliente\n"
-				  "\t0 - Retornar\n");
-		printf("\n\tEscolha uma opção: ");
-		scanf("%d", &opcao3);
-		
-		} while(opcao3 != 0);
+int pesquisarCliente(){
+	printf("Em Construção");
+	return 0;
 }
 
 
-void menuProduto(){
-	system("cls");
-	do{
-		printf ("\t= = = = = = = = = = = = = = = = = = = =\n"
-				"\t= C A D A S T R O  D O  P R O D U T O =\n"
-				"\t= = = = = = = = = = = = = = = = = = = =\n"
-				  "\n\t1 - Cadastrar Produto\n"
-				  "\t2 - Atualizar Produto\n"
-				  "\t3 - Pesquisar Produto\n"
-				  "\t4 - Deletar Produto\n"
-				  "\t0 - Retornar\n");
-		printf("\n\tEscolha uma opção: ");
-		scanf("%d", &opcao4);	
-		} while(opcao4 != 0);
+int atualizarCliente(){
+	printf("Em Construção");
+	return 0;
 }
 
-void menuControleDeEstoque(){
-	system("cls");
-	do{
-		printf ("\t= = = = = = = = = = = = = = = = = = = =\n"
-				"\t= C O N T R O L E  D E  E S T O Q U E =\n"
-				"\t= = = = = = = = = = = = = = = = = = = =\n"
-				  "\n\t1 - Efetuar Compra\n"
-				  "\t2 - Efetuar Venda\n"
-				  "\t3 - Pesquisar Compra\n"
-				  "\t4 - Pesquisar Venda\n"
-				  "\t5 - Atualizar Compra\n"
-				  "\t6 - Atualizar Venda\n"
-				  "\t0 - Retornar\n");
-		printf("\n\tEscolha uma opção: ");
-		scanf("%d", &opcao5);
-		} while(opcao5 != 0);
-	}
 
-void menuRelatorio(){
-	system("cls");
-	do{
-		printf ("\t= = = = = = = = = = = = = = = = = = = =\n"
-				"\t= C O N T R O L E  D E  E S T O Q U E =\n"
-				"\t= = = = = = = = = = = = = = = = = = = =\n"
-				  "\n\t1 - Relatório de Clientes\n"
-				  "\t2 - Relatório de Produtos\n"
-				  "\t3 - Relatório de Compras\n"
-				  "\t4 - Relatório de Vendas\n"
-				  "\t0 - Retornar\n");
-		printf("\n\tEscolha uma opção: ");
-		scanf("%d", &opcao6);
-		} while(opcao6 != 0);
+int deletarCliente(){
+	printf("Em Construção");
+	return 0;
 }
 
-void menuSobre(){
-	system("cls");
-	do{
-		printf ("\t= = = = = = = = = = = = = = = = = = = = =\n"
-				"\t= M U R A L  D E  I N F O R M A Ç Õ E S =\n"
-				"\t= = = = = = = = = = = = = = = = = = = = = \n"
-				  "\n\t1 - Quem somos\n"
-				  "\t2 - Contato\n"
-				  "\t3 - Informações do Programa\n"
-				  "\t0 - Retornar\n");
-		printf("\n\tEscolha uma opção: ");
-		scanf("%d", &opcao7);
-		} while(opcao7 != 0);
+
+///////////////////////////////////////////
+//// F U N Ç Õ E S  D O  P R O D U T O ////
+///////////////////////////////////////////
+
+int cadastrarProduto(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int pesquisarProduto(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int atualizarProduto(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int deletarProduto(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+/////////////////////////////////////////////////////////////////
+//// F U N Ç Õ E S  D O  C O N T R O L E  D E  E S T O Q U E ////
+/////////////////////////////////////////////////////////////////
+
+
+int cadastrarCompra(){
+	printf("Em Construção");
+	return 0;
+	
+}
+
+
+int pesquisarCompra(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int atualizarCompra(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int cadastrarVenda(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int pesquisarVenda(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int atualizarVenda(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+///////////////////////////////////////////////////
+//// F U N Ç Õ E S  D O S  R E L A T Ó R I O S ////
+///////////////////////////////////////////////////
+
+
+int relatorioDosClientes(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int relatorioDosProdutos(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int relatorioDasCompra(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int relatorioDasVenda(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+
+///////////////////////////////////////
+//// F U N Ç Õ E S  D O  S O B R E ////
+///////////////////////////////////////
+
+
+int quemSomos(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int contato(){
+	printf("Em Construção");
+	return 0;
+}
+
+
+int inforPrograma(){
+	printf("Em Construção");
+	return 0;
 }
