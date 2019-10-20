b09b84ac1614f2b34c5278ae65871640d0cfccdb
wanessabezerra <53651524+wanessabezerra@users.noreply.github.com>
2019-10-19 22:05:08 -0300

    Delete menuEstoqueDeVenda.h

--- menuEstoqueDeVenda.h
+++
@@ -1,9 +0,0 @@
-#ifndef MENUCONTROLEDEVENDAS_H_INCLUDED
-#define MENUCONTROLEDEVENDAS_H_INCLUDED
-
-int menuControleDeVendas();
-int cadastrarVenda();
-int pesquisarVenda();
-int atualizarVenda();
-
-#endif // MENUCONTROLEDEVENDAS_H_INCLUDED
